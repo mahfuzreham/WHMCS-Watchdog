9f204fcb2f91bcb88dda96e861ec516c5dd3c201
MD Mahfuz Reham <78415545+mahfuzreham@users.noreply.github.com>
2026-09-21 19:34:27 +0600

    Security and PHP 8.x compatibility hardening

--- modules/addons/Watchdog/lib/Settings.php
+++ modules/addons/Watchdog/lib/Settings.php
@@ -1,89 +1,119 @@
 <?php
 
-/**
- * Watchdog
- *
- * @package     WHMCS
- * @copyright   Katamaze
- * @link        https://katamaze.com
- * @author      Davide Mantenuto <info@katamaze.com>
- *
- */
-
 namespace WHMCS\Module\Addon\Watchdog;
 
 use WHMCS\Database\Capsule;
 
 class Settings
 {
-    function __construct()
+    private const ALLOWED_SETTINGS = [
+        'checkFrequency',
+        'actionsTaken',
+        'recipients',
+    ];
+
+    public function __construct()
     {
-        if ($_POST['setting']): $this->save(); endif;
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['setting'])) {
+            $this->save();
+        }
     }
 
-    function listing()
+    public function listing()
     {
-        foreach (Capsule::select(Capsule::raw('SELECT setting, value FROM tbladdonmodules WHERE module = "Watchdog"')) as $v)
-        {
-            if (in_array($v->setting, array('actionsTaken', 'recipients')))
-            {
-                if ($v->setting == 'actionsTaken')
-                {
-                    foreach (json_decode($v->value, true) as $key)
-                    {
-                        $output[$v->setting][$key] = true;
-                    }
+        $output = [
+            'actionsTaken' => [],
+            'recipients' => '',
+            'checkFrequency' => '24',
+            'lastRun' => '',
+        ];
 
-                    continue;
+        $rows = Capsule::table('tbladdonmodules')
+            ->where('module', 'Watchdog')
+            ->get(['setting', 'value']);
+
+        foreach ($rows as $row) {
+            if ($row->setting === 'actionsTaken') {
+                $decoded = json_decode((string) $row->value, true);
+                foreach (is_array($decoded) ? $decoded : [] as $key) {
+                    $output['actionsTaken'][(string) $key] = true;
                 }
-                else
-                {
-                    $v->value = json_decode($v->value, true);
-                    $v->value = implode(PHP_EOL, $v->value);
-                }
+            } elseif ($row->setting === 'recipients') {
+                $decoded = json_decode((string) $row->value, true);
+                $output['recipients'] = implode(PHP_EOL, is_array($decoded) ? $decoded : []);
+            } else {
+                $output[$row->setting] = (string) $row->value;
             }
-
-            $output[$v->setting] = $v->value;
         }
 
-        $output['WHMCSVersion'] = Capsule::select(Capsule::raw('SELECT value FROM tblconfiguration WHERE setting = "Version" LIMIT 1'))[0]->value;
+        $versionRow = Capsule::table('tblconfiguration')
+            ->where('setting', 'Version')
+            ->first(['value']);
+
+        $output['WHMCSVersion'] = $versionRow ? (string) $versionRow->value : '';
 
         return $output;
     }
 
-    function save()
+    private function requireAdminToken()
     {
-        if (in_array($_POST['setting'], array('actionsTaken')))
-        {
-            $_POST['value'] = json_encode($_POST['value']);
+        if (!function_exists('check_token') || !check_token('WHMCS.admin.default')) {
+            http_response_code(403);
+            exit('Invalid security token');
         }
-        elseif (in_array($_POST['setting'], array('recipients')))
-        {
-            $_POST['value'] = explode(PHP_EOL, $_POST['value']);
+    }
 
-            foreach ($_POST['value'] as $k => $v)
-            {
-                $v = trim($v);
+    private function save()
+    {
+        $this->requireAdminToken();
 
-                if (!filter_var($v, FILTER_VALIDATE_EMAIL))
-                {
-                    header('Location: addonmodules.php?module=Watchdog&view=Settings&heading=' . $_POST['heading'] . '&error=invalidemail');
-                    die();
-                }
+        $setting = isset($_POST['setting']) ? (string) $_POST['setting'] : '';
+        if (!in_array($setting, self::ALLOWED_SETTINGS, true)) {
+            http_response_code(400);
+            exit('Invalid setting');
+        }
 
-                $_POST['value'][$k] = trim($v);
+        $value = $_POST['value'] ?? '';
+
+        if ($setting === 'checkFrequency') {
+            $frequency = filter_var($value, FILTER_VALIDATE_INT, [
+                'options' => ['min_range' => 1, 'max_range' => 2160],
+            ]);
+
+            if ($frequency === false) {
+                http_response_code(422);
+                exit('Invalid check frequency');
             }
 
-            $_POST['value'] = json_encode(array_unique($_POST['value']));
-        }
-        elseif (in_array($_POST['setting'], array('checkFrequency')) AND (!is_int((int) $_POST['value'])))
-        {
-            header('Location: addonmodules.php?module=Watchdog&view=Settings&heading=' . $_POST['heading'] . '&error=integear');
+            $value = (string) $frequency;
+        } elseif ($setting === 'actionsTaken') {
+            $values = is_array($value) ? $value : [$value];
+            $allowed = ['neutralize', 'notify'];
+            $values = array_values(array_intersect($allowed, array_map('strval', $values)));
+            $value = json_encode(array_unique($values), JSON_THROW_ON_ERROR);
+        } elseif ($setting === 'recipients') {
+            $lines = preg_split('/\\R+/', (string) $value, -1, PREG_SPLIT_NO_EMPTY);
+            $emails = [];
+
+            foreach ($lines as $email) {
+                $email = trim($email);
+                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                    http_response_code(422);
+                    exit('Invalid email address');
+                }
+                $emails[] = $email;
+            }
+
+            $value = json_encode(array_values(array_unique($emails)), JSON_THROW_ON_ERROR);
         }
 
-        Capsule::table('tbladdonmodules')->where('setting', $_POST['setting'])->update(['value' => $_POST['value']]);
+        Capsule::table('tbladdonmodules')
+            ->where('module', 'Watchdog')
+            ->where('setting', $setting)
+            ->update(['value' => $value]);
 
-        header('Location: addonmodules.php?module=Watchdog&view=Settings&heading=' . $_POST['heading']);
-        die();
+        $heading = isset($_POST['heading']) ? (int) $_POST['heading'] : 1;
+        header('Location: addonmodules.php?module=Watchdog&view=Settings&heading=' . $heading);
+        exit;
     }
 }
