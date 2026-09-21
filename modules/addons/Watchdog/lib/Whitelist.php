9ff790712e0a50bcccbbecba388e7a0c818b61ea
MD Mahfuz Reham <78415545+mahfuzreham@users.noreply.github.com>
2026-09-21 19:34:36 +0600

    Security and PHP 8.x compatibility hardening

--- modules/addons/Watchdog/lib/Whitelist.php
+++ modules/addons/Watchdog/lib/Whitelist.php
@@ -1,9 +1,16 @@
 <?php
 
+namespace WHMCS\Module\Addon\Watchdog;
+
+use WHMCS\Database\Capsule;
+
 class Whitelist
 {
-    function listing()
+    public function listing()
     {
-        
+        return Capsule::table('wd_whitelist')
+            ->select(['path', 'notes'])
+            ->orderBy('path')
+            ->get();
     }
 }
